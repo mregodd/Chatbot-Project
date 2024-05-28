8151e037ee184dd54b2c5d5c3361ab8fc0a8ac5e
mregodd <derbedekegemen@gmail.com>
2024-05-28 14:46:18 +0300

    Add updated code with ML enhancements and fix for unknown intents

--- models/chatbot_model.py
+++ models/chatbot_model.py
@@ -1,26 +1,48 @@
-import numpy as np
+# -*- coding: utf-8 -*-
+import random
+import json
 import nltk
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
-import json
-import random
+import numpy as np
+import os
 
 lemmatizer = WordNetLemmatizer()
 nltk.download('punkt')
 nltk.download('wordnet')
 
-with open('data/intents.json', encoding='utf-8') as file:
-    intents = json.load(file)
+try:
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    intents_file_path = os.path.join(current_directory, '..', 'data', 'intents.json')
+    with open(intents_file_path, encoding='utf-8') as file:
+        intents = json.load(file)
+except json.JSONDecodeError as e:
+    print(f"JSONDecodeError: {e}")
+    exit()
+except FileNotFoundError as e:
+    print(f"FileNotFoundError: {e}")
+    exit()
 
-model = load_model('chatbot_model.h5')
+model = load_model(os.path.join(current_directory, 'chatbot_model.keras'))
 
 words = []
 classes = []
 documents = []
 ignore_words = ['?', '!', '.', ',']
 
+synonyms = {
+    "sağ ol": "sağol",
+    "teşekkürler": "teşekkür"
+}
+
+def normalize_sentence(sentence):
+    for word, norm in synonyms.items():
+        sentence = sentence.replace(word, norm)
+    return sentence
+
 for intent in intents['intents']:
     for pattern in intent['patterns']:
+        pattern = normalize_sentence(pattern)
         word_list = nltk.word_tokenize(pattern)
         words.extend(word_list)
         documents.append((word_list, intent['tag']))
@@ -33,6 +55,7 @@ words = sorted(list(set(words)))
 classes = sorted(list(set(classes)))
 
 def clean_up_sentence(sentence):
+    sentence = normalize_sentence(sentence)
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
@@ -49,9 +72,13 @@ def bow(sentence, words, show_details=True):
 def predict_class(sentence, model):
     p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
-    ERROR_THRESHOLD = 0.25
+    ERROR_THRESHOLD = 0.75
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     results.sort(key=lambda x: x[1], reverse=True)
+    
+    if len(results) == 0:
+        return [{"intent": "unknown", "probability": "1.0"}]
+    
     return_list = []
     for r in results:
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
@@ -59,12 +86,15 @@ def predict_class(sentence, model):
 
 def get_response(ints, intents_json):
     tag = ints[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if i['tag'] == tag:
-            result = random.choice(i['responses'])
-            break
-    return result
+    if tag == 'unknown':
+        return random.choice([i['responses'][0] for i in intents_json['intents'] if i['tag'] == 'unknown'])
+    else:
+        list_of_intents = intents_json['intents']
+        for i in list_of_intents:
+            if i['tag'] == tag:
+                result = random.choice(i['responses'])
+                break
+        return result
 
 def chatbot_response(msg):
     ints = predict_class(msg, model)
--- models/model_train.py
+++ models/model_train.py
@@ -1,27 +1,50 @@
-import json
+# -*- coding: utf-8 -*-
 import numpy as np
-import random
 import nltk
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import Dense, Dropout, Input
 from tensorflow.keras.optimizers import SGD
+import json
+import random
+import os
+import matplotlib.pyplot as plt
 
+lemmatizer = WordNetLemmatizer()
 nltk.download('punkt')
 nltk.download('wordnet')
 
-lemmatizer = WordNetLemmatizer()
+current_directory = os.path.dirname(os.path.abspath(__file__))
+intents_file_path = os.path.join(current_directory, '..', 'data', 'intents.json')
 
-with open('data/intents.json', encoding='utf-8') as file:
-    intents = json.load(file)
+try:
+    with open(intents_file_path, encoding='utf-8') as file:
+        intents = json.load(file)
+except json.JSONDecodeError as e:
+    print(f"JSONDecodeError: {e}")
+    exit()
+except FileNotFoundError as e:
+    print(f"FileNotFoundError: {e}")
+    exit()
 
 words = []
 classes = []
 documents = []
 ignore_words = ['?', '!', '.', ',']
 
+synonyms = {
+    "sağ ol": "sağol",
+    "teşekkürler": "teşekkür"
+}
+
+def normalize_sentence(sentence):
+    for word, norm in synonyms.items():
+        sentence = sentence.replace(word, norm)
+    return sentence
+
 for intent in intents['intents']:
     for pattern in intent['patterns']:
+        pattern = normalize_sentence(pattern)
         word_list = nltk.word_tokenize(pattern)
         words.extend(word_list)
         documents.append((word_list, intent['tag']))
@@ -33,10 +56,6 @@ words = sorted(list(set(words)))
 
 classes = sorted(list(set(classes)))
 
-print(len(documents), "documents")
-print(len(classes), "classes", classes)
-print(len(words), "unique lemmatized words", words)
-
 training = []
 output_empty = [0] * len(classes)
 
@@ -46,31 +65,37 @@ for doc in documents:
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
-    
+
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
-    
     training.append([bag, output_row])
 
 random.shuffle(training)
-training = np.array(training)
+training = np.array(training, dtype=object)
 
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
 
 model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Input(shape=(len(train_x[0]),)))
+model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-# Modeli derleyin
-sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-# Modeli e�itin
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('chatbot_model.h5', hist)
+model.save(os.path.join(current_directory, 'chatbot_model.keras'), hist)
 
-print("Model olu�turuldu ve kaydedildi")
+plt.plot(hist.history['accuracy'])
+plt.plot(hist.history['loss'])
+plt.title('Model accuracy & loss')
+plt.ylabel('Accuracy / Loss')
+plt.xlabel('Epoch')
+plt.legend(['Accuracy', 'Loss'], loc='upper left')
+plt.show()
+
+print("Model oluşturuldu ve kaydedildi")
