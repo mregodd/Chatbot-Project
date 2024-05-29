d6c460da845809e66b2e5e4b719717013ea830f2
mregodd <derbedekegemen@gmail.com>
2024-05-29 14:08:58 +0300

    fix for test_chatbot

--- models/trainer.py
+++ models/trainer.py
@@ -1,9 +1,6 @@
 import os
 import sys
-import time
-current_directory = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(current_directory, '..'))
-
+import json
 import numpy as np
 import random
 import nltk
@@ -11,8 +8,13 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Input
 from tensorflow.keras.optimizers import SGD
 import matplotlib.pyplot as plt
-from data_loader import load_intents
-from utils import tokenize_and_lemmatize, ignore_words, lemmatizer
+
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(current_directory, '..'))
+
+from models.data_loader import load_intents
+from models.utils import tokenize_and_lemmatize, ignore_words, lemmatizer
 
 nltk.download('punkt')
 nltk.download('wordnet')
@@ -54,6 +56,9 @@ def train_model():
     train_x = list(training[:, 0])
     train_y = list(training[:, 1])
 
+    print(f"Number of words (features): {len(words)}") 
+    print(f"Classes: {classes}") 
+
     model = Sequential()
     model.add(Input(shape=(len(train_x[0]),)))
     model.add(Dense(128, activation='relu'))
@@ -67,7 +72,7 @@ def train_model():
 
     hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
     
-    model.save(os.path.join(current_directory, 'chatbot_model.keras'))
+    model.save(os.path.join(current_directory, '..', 'chatbot_model.keras')) 
 
     plt.plot(hist.history['accuracy'])
     plt.plot(hist.history['loss'])
